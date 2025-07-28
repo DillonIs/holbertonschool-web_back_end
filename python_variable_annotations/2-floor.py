7c57b399afffd81e151f6092b49461873be78b45
DillonIs <dillonis826@gmail.com>
2025-07-29 09:48:47 +1000

    Type Annotation floor

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Type Annotation: floor
+Passes a float, defined as n, as an argument
+Converts n into an integer using floor"""
+
+
+def floor(n: float) -> int:
+    """Returns n as an integer"""
+    return int(n)
