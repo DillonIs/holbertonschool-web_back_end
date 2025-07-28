0e50c86e520d406267dd26e793b23e191733b856
DillonIs <dillonis826@gmail.com>
2025-07-29 09:26:37 +1000

    Adding floats with type annotations

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+"""Type annotation to add two float values
+a float and b float passed as arguments"""
 
 def add(a: float, b: float) -> float:
+    """Returns the sum of a and b"""
     return a + b
