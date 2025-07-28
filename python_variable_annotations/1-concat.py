ed83fd3eab28d2d3aabad504144a15069668cf2c
DillonIs <dillonis826@gmail.com>
2025-07-29 09:38:07 +1000

    Type annotations string concat

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Type annotation: Concat
+Passes str1 and str2 as arguments"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Returns concat of str1 and str2"""
+    return str1 + str2
