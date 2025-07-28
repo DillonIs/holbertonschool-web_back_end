254af8d36aa11ecc38c4886f6d599c019642d239
DillonIs <dillonis826@gmail.com>
2025-07-29 09:54:39 +1000

    Type Annotation convert float to string

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Type annotations to str
+takes a float defined as n
+converts n into str"""
+
+
+def to_str(n: float) -> str:
+    """Returns string representation of n"""
+    return str(n)
