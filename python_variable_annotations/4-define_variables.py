c92c855f1e2f3597c00c0edbb1bc9341d885d711
DillonIs <dillonis826@gmail.com>
2025-07-30 09:24:50 +1000

    Define variables using annotations

---
+++ python_variable_annotations/4-define_variables.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Define variables and annotate the following
+a integer with a value of 1
+pi float with a value of 3.14
+i_understand_annotations bool with a value of True
+school string with a value of Holberton"""
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
