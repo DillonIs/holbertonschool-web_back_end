1001670fb01cc8e1880ebe46cf6936e274247ed1
DillonIs <dillonis826@gmail.com>
2025-08-11 10:52:49 +1000

    simple pagination

---
+++ pagination/1-simple_pagination.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+"""Simple helper function named index_range"""
+from typing import Tuple, List
+import csv
+import math
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """Helper function that takes two integers and returns a Tuple
+    with a start index and an end index"""
+
+    start = (page - 1) * page_size
+    end = page * page_size
+
+    return (start, end)
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Locates the pagination in a dataset"""
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page > 0
+
+        self.dataset()
+
+        if self.__dataset is None:
+            return []
+
+        ind_range = index_range(page, page_size)
+        data = self.__dataset[ind_range[0]:ind_range[1]]
+        return data
