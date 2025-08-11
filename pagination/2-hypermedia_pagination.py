f98a2df2f4353e2deef8670115fa915e3403ed15
DillonIs <dillonis826@gmail.com>
2025-08-11 11:10:22 +1000

    Hypermedia pagination

---
+++ pagination/2-hypermedia_pagination.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+"""Simple helper function named index_range"""
+from typing import Tuple, List
+import csv
+from math import ceil
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """Helper function that takes two integers and returns a Tuple
+    with a start index and an end index"""
+
+    start = (page - 1) * page_size
+    end = page * page_size
+
+    return (start, end)
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Locates the pagination in a dataset"""
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page > 0
+
+        self.dataset()
+
+        if self.__dataset is None:
+            return []
+
+        ind_range = index_range(page, page_size)
+        data = self.__dataset[ind_range[0]:ind_range[1]]
+        return data
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
+        """Provides more details about the dataset"""
+        data = self.get_page(page, page_size)
+
+        data_set = self.__dataset
+        len_set = len(data_set) if data_set else 0
+
+        total_pages = ceil(len_set / page) if data_set else 0
+
+        if not data:
+            page_size = 0
+        else:
+            page_size = len(data)
+
+        prev_page = page - 1 if page > 1 else None
+        next_page = page + 1 if page < total_pages else None
+
+        hyper = {
+            'page_size': page_size,
+            'page': page,
+            'data': data,
+            'next_page': next_page,
+            'prev_page': prev_page,
+            'total_pages': total_pages
+        }
+
+        return hyper
