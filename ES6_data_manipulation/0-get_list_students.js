fb9a035d25638b470dc9689387eba92120e19004
DillonIs <dillonis826@gmail.com>
2025-08-27 08:55:26 +1000

    ES6 return an array

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,8 +1,19 @@
-export default function getListStudents() {
-  const studentsArray = [
-    { id: 1, firstname: 'Guillaume', location: 'San Francisco'},
-    { id: 2, firstname: 'James', location: 'Columbia'},
-    { id: 5, firstname: 'Serena', location: 'San Francisco'},
+const getListStudents = () => [
+    {
+      id: 1,
+      firstname: 'Guillaume',
+      location: 'San Francisco'
+    },
+    {
+      id: 2,
+      firstname: 'James',
+      location: 'Columbia'
+    },
+    {
+      id: 5,
+      firstname: 'Serena',
+      location: 'San Francisco'
+    },
   ];
-  return studentsArray;
-}
+
+export default getListStudents;
