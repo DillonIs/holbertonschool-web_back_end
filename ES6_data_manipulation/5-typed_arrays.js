5c29b2afc105f894a84372bd2e19d26f10921859
DillonIs <dillonis826@gmail.com>
2025-08-27 10:01:02 +1000

    Array buffers ES6

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,7 +1,7 @@
 export default function createInt8TypedArray(length, position, value) {
   const buffer = new ArrayBuffer(length);
-  const view = new Int8Array(buffer);
-  if (position > view.length) {
+  const int8Array = new Int8Array(buffer);
+  if (position > int8Array.length) {
     throw new Error('Position outside of range');
   }
   view[position] = value;
