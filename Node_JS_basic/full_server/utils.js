b9cf0281f7472df4fd204154b7fcd2a3054becaf
DillonIs <dillonis826@gmail.com>
2025-09-05 13:34:06 +1000

    Creating a full Express server

---
+++ Node_JS_basic/full_server/utils.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+
+const readDatabase = (filepath) => new Promise((resolve, reject) => {
+    fs.readFile(filepath, 'utf-8', (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+        return;
+      }
+
+      const lines = data.split('\n').filter((line) => line.trim() !== '');
+      const students = {};
+
+      lines.slice(1).forEach((line) => {
+        const [firstname, , , field] = line.split(',');
+
+        if (!students[field]) {
+          students[field] = [];
+        }
+        students[field].push(firstname);
+      });
+
+      resolve(students);
+    });
+  });
+
+module.exports = readDatabase;
